26f271884f4f4c6587dd779616c4e6127ba5ec5b
John Slike <jslike1@kent.edu>
2018-02-05 15:57:49 -0500

    Overhauled Metronome to utilize timers/event handlers

---
+++ Metronome/Metronome/Classes/MetronomeClass.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Metronome.Classes
+{
+    public class MetronomeClass
+    {
+
+        public int BeatsPerMeasure { get; set; }
+
+        public int NoteLength { get; set; }
+
+        public int CurrentBeat { get; set; }
+
+        public MetronomeClass()
+        {
+            this.CurrentBeat = 1;
+        }
+       
+
+
+        public void MetronomeTick()
+        {
+            if (CurrentBeat != 1)
+            {
+                Console.Beep(440, 100);
+                if (CurrentBeat + 1 <= BeatsPerMeasure)
+                {
+                    CurrentBeat += 1;
+                }
+                else
+                {
+                    CurrentBeat = 1;
+                }
+            }
+            else
+            {
+                Console.Beep(880, 100);
+                if (CurrentBeat + 1 <= BeatsPerMeasure)
+                {
+                    CurrentBeat += 1;
+                }
+                else
+                {
+                    CurrentBeat = 1;
+                }
+            }
+        }
+
+
+        //static void RunMetronome()
+        //{
+        //    int currentBeat = 1;
+
+
+
+        //    while (currentBeat <= beatsPerMeasure)
+        //    {
+        //        if (currentBeat == 1)
+        //        {
+        //            Console.Beep(880, noteLength);
+        //            Thread.Sleep(noteLength);
+        //            currentBeat += 1;
+        //        }
+        //        else if (currentBeat != 1)
+        //        {
+        //            Console.Beep(440, noteLength);
+        //            Thread.Sleep(noteLength);
+        //            currentBeat += 1;
+        //        }
+        //    }
+        //}
+    }
+}
--- Metronome/Metronome/Program.cs
+++ Metronome/Metronome/Program.cs
@@ -1,76 +1,96 @@
-﻿using System;
+﻿using Metronome.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Threading;
+using System.Timers;
 
-namespace ConsoleApp1
+namespace Metronome
 {
     class Program
     {
+
+        static MetronomeClass metronome = new MetronomeClass();
+
         static void Main(string[] args)
         {
             while (true)
-            { 
-            Console.WriteLine("What is your tempo? (bmp): ");
-            string answer = Console.ReadLine();
-            int tempo = Convert.ToInt32(answer);
+            {
+                Console.WriteLine("What is your tempo? (bpm): ");
+                string answer = Console.ReadLine();
+                int tempo = Convert.ToInt32(answer);
 
-            Console.WriteLine("What is your time signature? (2/4, 3/4, 4/4?): ");
-            string timeSignature = Console.ReadLine();
-
-
-            Console.WriteLine("Run The Metronome? (y/n): ");
-            answer = Console.ReadLine();
-
-            int noteMil = 60000 / tempo;
-
-            int noteRest = noteMil / 2;
+                Console.WriteLine("What is your time signature? (?/4, ?/8, etc?): ");
+                answer = Console.ReadLine();
+                int beatsPerMeasure = Convert.ToInt32(answer);
+                metronome.BeatsPerMeasure = beatsPerMeasure;
 
 
+                Console.WriteLine("Run The Metronome? (y/n): ");
+                answer = Console.ReadLine();
 
                 if (answer == "y")
                 {
-                    if (timeSignature == "2/4")
-                    {
-                        while (true)
-                        {
-                            Console.Beep(880, noteRest);
-                            Thread.Sleep(noteRest);
-                            Console.Beep(440, noteRest);
-                            Thread.Sleep(noteRest);
 
-                        }
-                    }
-                    else if (timeSignature == "3/4")
+
+                    int noteMil = 60000 / tempo;
+                    
+
+                    int noteRest = noteMil / 2;
+                    metronome.NoteLength = noteRest;
+
+
+                    Timer tick = new Timer(noteMil);
+
+                    tick.Elapsed += EventHandler;
+
+                    tick.AutoReset = true;
+                    tick.Enabled = true;
+
+                    Console.WriteLine("Press Any Key to Quit");
+
+                    answer = Console.ReadKey().ToString();
+                    Console.Clear();
+                    if (answer != "")
                     {
-                        while (true)
-                        {
-                            Console.Beep(880, noteRest);
-                            Thread.Sleep(noteRest);
-                            Console.Beep(440, noteRest);
-                            Thread.Sleep(noteRest);
-                            Console.Beep(440, noteRest);
-                            Thread.Sleep(noteRest);
-                        }
-                    }
-                    else if (timeSignature == "4/4")
-                    {
-                        while (true)
-                        {
-                            Console.Beep(880, noteRest);
-                            Thread.Sleep(noteRest);
-                            Console.Beep(440, noteRest);
-                            Thread.Sleep(noteRest);
-                            Console.Beep(440, noteRest);
-                            Thread.Sleep(noteRest);
-                            Console.Beep(440, noteRest);
-                            Thread.Sleep(noteRest);
-                        }
+                        tick.Dispose();
                     }
                 }
+
+
             }
+
+
         }
+        
+        static void EventHandler(Object source, ElapsedEventArgs e)
+        {
+            metronome.MetronomeTick();
+        }
+
+        //static void RunMetronome(int beatsPerMeasure, int noteRest, int currentBeat)
+        //{
+
+        //    Console.Beep(440, noteRest);
+
+            
+            
+            //while (currentBeat <= beatsPerMeasure)
+            //if (currentBeat == 1)
+            //{
+            //    Console.Beep(880, noteRest);
+            //    currentBeat += 1;
+
+            //}
+            //else if (currentBeat != 1)
+            //{
+            //    Console.Beep(440, noteRest);
+            //    currentBeat += 1;
+
+            //}
+
+        //}
+
     }
 }
